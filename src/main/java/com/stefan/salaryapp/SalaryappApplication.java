e14101ef4fddfefccd6b0b24b4eafb4872815de2
Dragomiroiu Stefan <dragomiroiustefan@gmail.com>
2021-06-03 13:11:10 +0300

    Initial commit: Spring project initialised

---
+++ src/main/java/com/stefan/salaryapp/SalaryappApplication.java
@@ -0,0 +1,13 @@
+package com.stefan.salaryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaryappApplication.class, args);
+	}
+
+}
